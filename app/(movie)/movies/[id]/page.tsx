ac01c5d98a50111d90f7d4af7880f3e532147a81
kks2140501 <kks2140501@naver.com>
2024-11-06 01:39:43 +0900

    generateMetadata같은 함수에 전달되는 params 가 비동기로 처리할 수 있도록 Promise 감싸기

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -2,18 +2,45 @@ import { Suspense } from "react";
 import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
+// interface IParams {
+//   params: { id: string };
+// }
+
 interface IParams {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
-export async function generateMetadata({ params: { id } }: IParams) {
+// export async function generateMetadata({ params: { id } }: IParams) {
+//   const movie = await getMovie(id);
+//   return {
+//     title: movie.title,
+//   };
+// }
+
+export async function generateMetadata({ params }: IParams) {
+  const { id } = await params; // 비동기적으로 params를 기다림.
   const movie = await getMovie(id);
   return {
     title: movie.title,
   };
 }
 
-export default async function MovieDetailPage({ params: { id } }: IParams) {
+// export default async function MovieDetailPage({ params: { id } }: IParams) {
+//   return (
+//     <div>
+//       <Suspense fallback={<h1>Loading movie info</h1>}>
+//         <MovieInfo id={id} />
+//       </Suspense>
+//       <Suspense fallback={<h1>Loading movie videos</h1>}>
+//         <MovieVideos id={id} />
+//       </Suspense>
+//     </div>
+//   );
+// }
+
+export default async function MovieDetailPage({ params }: IParams) {
+  const { id } = await params; // 비동기적으로 params를 기다립니다
+
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
